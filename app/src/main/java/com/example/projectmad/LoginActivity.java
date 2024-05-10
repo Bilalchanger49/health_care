06b4e4289dd82dbc60614eeb1dc455e732249b8c
Bilalchanger49 <bilalchanger49@gmail.com>
2024-05-10 12:47:18 +0500

    testing

--- app/src/main/java/com/example/projectmad/LoginActivity.java
+++ app/src/main/java/com/example/projectmad/LoginActivity.java
@@ -35,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 startActivity(new Intent(LoginActivity.this,HomeActivity.class));
                 /*String username= edUsername.getText().toString();
                 String password= edPassword.getText().toString();
