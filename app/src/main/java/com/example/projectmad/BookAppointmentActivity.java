c30699f7038e5e331473c60972373010934fef92
mdjunaid56 <junaid17chk@gmail.com>
2024-05-10 11:51:30 +0500

    nothing

--- app/src/main/java/com/example/projectmad/BookAppointmentActivity.java
+++ app/src/main/java/com/example/projectmad/BookAppointmentActivity.java
@@ -65,7 +65,6 @@ public class BookAppointmentActivity extends AppCompatActivity {
         int year=cal.get(Calendar.YEAR);
         int month=cal.get(Calendar.MONTH);
         int day=cal.get(Calendar.DAY_OF_MONTH);
-
         int style = AlertDialog.THEME_HOLO_DARK;
         datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
         datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis()+86400000);
