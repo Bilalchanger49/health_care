a325c655defab59cb2417670a0f0bdaefb0460c5
mdjunaid56 <junaid17chk@gmail.com>
2024-05-14 00:25:06 +0500

    BookAppointmentActivity.java adding date and time functionality for booking appointment

--- app/src/main/java/com/example/projectmad/BookAppointmentActivity.java
+++ app/src/main/java/com/example/projectmad/BookAppointmentActivity.java
@@ -5,6 +5,8 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -24,6 +26,7 @@ public class BookAppointmentActivity extends AppCompatActivity {
     TextView tv;
     private DatePickerDialog datePickerDialog;
     private TimePickerDialog timePickerDialog;
+    private Button dateButton, timeButton,btnBook,btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,10 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed2=findViewById(R.id.editTextAppAddress);
         ed3=findViewById(R.id.editTextAppContactNumber);
         ed4=findViewById(R.id.editTextAppFees);
+        dateButton= findViewById(R.id.buttonAppDate);
+        timeButton = findViewById(R.id.buttonAppTime);
+        btnBook = findViewById(R.id.buttonBookAppointment);
+        btnBack = findViewById(R.id.buttonAppBack);
 //Text isn't editable
         ed1.setKeyListener(null);
         ed2.setKeyListener(null);
@@ -52,13 +59,46 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed2.setText(address);
         ed3.setText(contact);
         ed4.setText("Cons Fees"+fees+"/-");
+
+        //datepicker
+        initDatePicker();
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+
+        //timepicker
+        initTimePicker();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePickerDialog.show();
+            }
+        });
+
+        //Back Button
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BookAppointmentActivity.this,FindDoctorActivity.class));
+            }
+        });
+      //Book Button
+      btnBook.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+          }
+      });
     }
     private  void initDatePicker(){
         DatePickerDialog.OnDateSetListener dateSetListener= new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                 i1=i1+1;
-//               dateButton.setText(i2+"/"+i1+"/"+i);
+              dateButton.setText(i2+"/"+i1+"/"+i);
             }
         };
         Calendar cal = Calendar.getInstance();
@@ -68,5 +108,21 @@ public class BookAppointmentActivity extends AppCompatActivity {
         int style = AlertDialog.THEME_HOLO_DARK;
         datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
         datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis()+86400000);
+    }
+
+    //Time
+    private void initTimePicker(){
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                timeButton.setText(i+":"+i1);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int hrs = cal.get(Calendar.HOUR);
+        int mins = cal.get(Calendar.MINUTE);
+
+        int style= AlertDialog.THEME_HOLO_DARK;
+        timePickerDialog = new TimePickerDialog(this,style,timeSetListener,hrs,mins,true);
     }
 }
